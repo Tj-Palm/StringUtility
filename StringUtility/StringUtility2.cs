086487a2bb19b683dffb4403672db9b7856fa1b2
Tj-Palm <48101800+Tj-Palm@users.noreply.github.com>
2020-03-02 12:33:19 +0100

    StringUtility2 & TestStringUtility2 added

---
+++ StringUtility/StringUtility2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringUtility
+{
+    public class StringUtility2
+    {
+        public int CountOccurences(string stringToCheck, string stringToFind)
+        {
+            int result = 0;
+
+            if (stringToCheck == null || stringToFind == null)
+            {
+                return -1;
+            }
+
+            if (stringToCheck == "" || stringToFind == "")
+            {
+                return 0;
+            }
+
+            if (stringToFind.Length > 1)
+            {
+                for (int i = 0; i < stringToCheck.Length-stringToFind.Length; i++)
+                {
+                    for (int t = 0; t < stringToFind.Length; t++)
+                    {
+                        if (stringToCheck[i+t].ToString().ToUpper() == stringToFind[t].ToString().ToUpper())
+                        {
+                            result++;
+                        }
+                        else
+                        {
+                            result = 0;
+                        }
+                    }
+
+                }
+
+                return result / stringToFind.Length;
+            }
+            else
+            {
+                foreach (var character in stringToCheck)
+                {
+                    if (character.ToString().ToUpper() == stringToFind[0].ToString().ToUpper())
+                    {
+                        result++;
+                    }
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
--- TestStringUtility/TestStringUtility2.cs
+++ TestStringUtility/TestStringUtility2.cs
@@ -1,69 +1,67 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StringUtility;
 
 namespace TestStringUtility
 {
     [TestClass]
-    public class TestStringUtility
+    public class TestStringUtility2
     {
-        private StringUtility.StringUtility classUnderTest;
+        private StringUtility.StringUtility2 classUnderTest;
 
         [TestInitialize]
         public void Init()
         {
-            classUnderTest = new StringUtility.StringUtility();
-        }
-
-        [TestMethod]
-        public void ShouldFindOneYInMysterious()
-        {
-            string stringToCheck = "Mysterious";
-            char stringToFind = 'y';
-            int expectedResult = 1;
-
-            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [TestMethod]
-        public void ShouldFindTwoSInMysterious()
-        {
-            string stringToCheck = "Mysterious";
-            char stringToFind = 's';
-            int expectedResult = 2;
-
-            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [TestMethod]
-        public void ShouldBeCaseSensitive()
-        {
-            string stringToCheck = "Arabia";
-            char stringToFind = 'A';
-            int expectedResult = 3;
-
-            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
-            Assert.AreEqual(expectedResult, actualResult);
+            classUnderTest = new StringUtility.StringUtility2();
         }
 
         [TestMethod]
         public void ShouldBeNull()
         {
             string stringToCheck = null;
-            char stringToFind = 'A';
+            string stringToFind = "A";
             int expectedResult = -1;
 
             int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
             Assert.AreEqual(expectedResult, actualResult);
         }
         [TestMethod]
-        public void ShouldBeSymbol()
+        public void ShouldBeEmptyToCheck()
         {
-            string stringToCheck = "email@email.dk";
-            char stringToFind = '@';
+            string stringToCheck = "";
+            string stringToFind = "A";
+            int expectedResult = 0;
+
+            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void ShouldBeEmptyToFind()
+        {
+            string stringToCheck = "Arabia";
+            string stringToFind = "";
+            int expectedResult = 0;
+
+            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void ShouldBeMoreThan1StringToFind()
+        {
+            string stringToCheck = "Arabia";
+            string stringToFind = "Ara";
             int expectedResult = 1;
+
+            int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void ShouldBe()
+        {
+            string stringToCheck = "ArabiaArabiaArabia";
+            string stringToFind = "AbabiaArabiaArabia";
+            int expectedResult = 0;
 
             int actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
             Assert.AreEqual(expectedResult, actualResult);
