0469dd7d51ff2aebd6b84e5388014b0c94e6a75b
Tj-Palm <48101800+Tj-Palm@users.noreply.github.com>
2020-03-02 10:39:39 +0100

    Finish refactor.

---
+++ StringUtility/StringUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringUtility
+{
+    public class StringUtility
+    {
+        public int CountOccurences(string stringToCheck, char stringToFind)
+        {
+            int result = 0;
+
+            if (stringToCheck == null || stringToFind == null)
+            {
+                return -1;
+            }
+
+            foreach (var character in stringToCheck)
+            {
+                if (character.ToString().ToUpper() == stringToFind.ToString().ToUpper())
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
